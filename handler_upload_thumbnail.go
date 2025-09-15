df6ce37c0a68634131e298b852b4446d6a7f1d68
sudonetizen <sardortolibjonov1039@gmail.com>
2025-09-15 15:41:52 +0500

    file storage

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+    "io"
+    "os"
 	"fmt"
+    "strings"
 	"net/http"
+    "path/filepath"
 
-	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +36,61 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+    const maxMemory = 10 << 20 
+    r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+    // getting thumbnail
+    file, header, err := r.FormFile("thumbnail")
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+        return
+    }
+    defer file.Close()
+
+    // content type 
+    contentType := header.Header.Get("Content-Type")
+    fmt.Println(contentType)
+
+    // getting video metadata 
+    vde, err := cfg.db.GetVideo(videoID)
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Unable to find video", err)
+        return
+    }
+
+    // checking userID with userID at video's metadata
+    if userID != vde.UserID {
+        respondWithError(w, http.StatusUnauthorized, "Unable to find video", err)
+        return
+    }
+
+    // saving as a file 
+    fileFormat := strings.ReplaceAll(contentType, "image/", "")
+    fileFormat = strings.TrimSpace(fileFormat)
+    path := filepath.Join("assets", videoIDString + "." + fileFormat)
+
+    fle, err := os.Create(path)
+    if err != nil {
+        respondWithError(w, 500, "error with creating path", err)
+        return
+    }
+    defer fle.Close()
+    
+    _, err = io.Copy(fle, file)
+    if err != nil {
+        respondWithError(w, 500, "error with copying file", err)
+        return
+    }
+  
+    // updating video 
+    dataURL := fmt.Sprintf("http://localhost:%v/assets/%v.%v", cfg.port, videoIDString, fileFormat)
+    vde.ThumbnailURL = &dataURL
+
+    err = cfg.db.UpdateVideo(vde)
+    if err != nil {
+        respondWithError(w, 500, "error with converting", err)
+        return
+    }
+
+	respondWithJSON(w, http.StatusOK, vde)
 }
